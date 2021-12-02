e62d0610fad5daeb53a43eaabb1f1fd2e53d110a
Nikolay Nikolaenko <niko.dr@gmail.com>
2021-12-02 11:05:12 +0200

    Initial Commit

---
+++ TextOnFonChanged/TextOnFonChangedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextOnFonChangedApp.swift
+//  TextOnFonChanged
+//
+//  Created by Nikolay Nikolayenko on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TextOnFonChangedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
